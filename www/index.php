9aefb69babe381d6759661d430f3e68b52cd642b
TCSE <talik@tcse-cms.com>
2016-12-05 17:54:27 +0300

    v1.3 добавлено отображение иконок файла и папки напротив соответствующих пунктов в списке.

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
 =====================================================
  php_folder_viewer - by TCSE
 -----------------------------------------------------
- Версия: v1.2 от 2016-11-13
+ Версия: v1.3 от 2016-12-05
 -----------------------------------------------------
  Copyright (c) 2016 TCSE http://tcse-cms.com/
 =====================================================
@@ -19,7 +19,7 @@
 $col="<br><br><p align=\"center\"><font class=\"well\">tcse-cms.com &copy; 2016.</font></p>";
 $self=basename($PHP_SELF);
 $dn=opendir('.');
-$dir=dirname("$SERVER_NAME$PHP_SELF");
+//$dir=dirname("$SERVER_NAME$PHP_SELF");
 print"
 
 <html>
@@ -41,23 +41,41 @@ print"
       <div class=\"col-xs-12 col-sm-6 col-md-4 col-lg-4\">
         <div class=\"list-group\">
           <p><br></p>
-          ";
+";
+
           while($file=readdir($dn)):
              switch($file):
                case(".."):
                  print"<a href=\"$file\" class=\"list-group-item list-group-item-info\" title=\"На уровень выше.\"><font face=\"tahoma\" color=\"#808080\" size=\"+2\"><b>$SERVER_NAME <i class=\"fa fa-level-up fa-2x\"></i> </b></font></a><Font color=\"#0000080\" size=\"+2\">$dir</font>\n";break;
-               case("."):
+              case("."):
                  print""; break;       //  не выдавать ссылки наверх.
-               case("$self"):
+              case("$self"):
                  print"" ;break;       // не показывать этот файл в списке
-               case("index.html"):
-                 print"" ;break;       // не показывать index.html
-               default:
-                print"<a href=\"$file\" class=\"list-group-item\" title=\"Download.\">$file</a>\n";break;
+              
+              case("index.php"):
+                 print"" ;break;       // не показывать index.php
+
+
+              default:
+        if(is_dir($file) == true){
+                    // это Файл
+                    print "<a href=\"".$file."\" class=\"list-group-item\" title=\"Download.\">
+                    <i class=\"fa fa-folder-o\" aria-hidden=\"true\">&nbsp;</i>"
+                    . $file
+                    . "</a>\n";
+                }else{
+                    // это папка
+                    print "<a href=\"".$file."\" class=\"list-group-item\" title=\"Download.\">
+                    <i class=\"fa fa-file\" aria-hidden=\"true\">&nbsp;</i>"
+                    . $file
+                    . "</a>\n";
+                }
+                break;
+
+        break;
              endswitch;
           endwhile;
-          print"
-          $col <br>
+          print $col ."<br>
         </div>
       </div>
     </div>
